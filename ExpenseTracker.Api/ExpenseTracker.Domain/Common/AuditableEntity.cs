17f15d54f66a3ec14310cae10b1c139722ca018d
Jamshidbek <161879703+jamshidbek7375@users.noreply.github.com>
2024-12-28 13:10:16 +0500

    Update AuditableEntity.cs

--- ExpenseTracker.Api/ExpenseTracker.Domain/Common/AuditableEntity.cs
+++ ExpenseTracker.Api/ExpenseTracker.Domain/Common/AuditableEntity.cs
@@ -3,7 +3,7 @@
 public abstract class AuditableEntity : EntityBase
 {
     public DateTime CreatedAtUtc { get; set; }
-    public string? CreatedBy { get; set; }
+    public required string CreatedBy { get; set; }
     public DateTime? LastUpdatedAtUtc { get; set; }
     public string? LastUpdatedBy { get; set; }
 }
